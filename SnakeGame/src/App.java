1471493d659e0c8b4ba10dcc9ac42aa5c6328f52
Emil Thostrup Pedersen <steamman0103@gmail.com>
2022-11-14 12:33:24 +0100

    Vi skal indsætte billiede færdig med arr[]
    Co-authored-by: MattisK <MattisK@users.noreply.github.com>
    Co-authored-by: mjmikkel04 <mjmikkel04@users.noreply.github.com>

--- SnakeGame/src/App.java
+++ SnakeGame/src/App.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
-//import java.awt.*;
-//import java.lang.Math.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.*;
+import java.lang.Math.*;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
 
-        // variables needed in defining frame
+        definePositionalArrays();
+
+        // variables needed in defining frames
         int widthFrame = 801;
         int heightFrame = 828;
 
@@ -24,11 +30,70 @@ public class App {
         Grid Gpanel = new Grid();
         frame.add(Gpanel);
 
+        // draw(g, int x, int y);
+
+        // frame.add(callSquare);
+
+        // mad();
         // making closing the window possible
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // initializing the window
         frame.setVisible(true);
+
+    }
+
+    public static void drawSquare(Graphics g, int x, int y) {
+        g.setColor(Color.RED);
+        g.drawRect(x, y, 50, 50);
+        g.fillRect(x, y, 50, 50);
+        g.setColor(Color.BLACK);
+    }
+
+    public static void nytmad() {
+        int xi, yi, x = 10, y = x, w = 30, h = w, offset = 2;
+        int grid = 13, many = grid * grid;
+
+        color col1 = (Color.RED);
+        color col2 = (Color.blue);
+        boolean switchcol = true;
+        PImage myPicture;
+
+        boolean[] apple = new boolean[];
+        BufferedImage myPicture = ImageIO.read(new File("Apple snakegame.png"));
+        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+        add(picLabel);
+        apple[(int) Random(many)] = true;
+    }
+
+    public static void definePositionalArrays() {
+        int æ = 0;
+        int[] posXArr = new int[256];
+        int[] posYArr = new int[256];
+        for (int å = 0; å < posXArr.length; å++) {
+
+            if (æ == 16) {
+                æ = 0;
+                posXArr[å] = æ * 50;
+                æ++;
+            } else {
+                posXArr[å] = æ * 50;
+                æ++;
+            }
+
+        }
+        for (int å = 0; å < posYArr.length; å++) {
+
+            if (æ == 16) {
+                æ = 0;
+                posYArr[å] = æ * 50;
+                æ++;
+            } else {
+                posYArr[å] = æ * 50;
+                æ++;
+            }
+
+        }
 
     }
 }
