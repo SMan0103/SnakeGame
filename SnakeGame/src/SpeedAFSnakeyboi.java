bef5cabae0c921ab0ee6cf9d09a1228328d6beaa
Emil Thostrup Pedersen <steamman0103@gmail.com>
2022-11-24 08:16:48 +0100

    prøver at få movement ind
    Co-authored-by: mjmikkel04 <mjmikkel04@users.noreply.github.com>
    Co-authored-by: MattisK <MattisK@users.noreply.github.com>

--- SnakeGame/src/SpeedAFSnakeyboi.java
+++ SnakeGame/src/SpeedAFSnakeyboi.java
@@ -2,40 +2,51 @@
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class SpeedAFSnakeyboi extends KeyAdapter {
+public class SpeedAFSnakeyboi{
 
-    public boolean up = true;
-    public boolean down = false;
-    public boolean left = false;
-    public boolean right = false;
+    public static int rotation = 1;
+    public static int posx = 0;
+    public static int posy = 0;
+    // 1 is up, 2 is down, 3 is right, 4 is left
 
     public void keyPressed(KeyEvent e) {
 
+        System.out.println("doodoo");
+
+        /*
+         * int key = e.getKeyCode();
+         * if(ey == KeyEvent.VK_W) {
+         * System.out.println("penis");
+         * }
+         */
+
         int key = e.getKeyCode();
-        if ((key == KeyEvent.VK_W) && (down = false)) {
-            left = false;
-            right = false;
-            up = true;
+        if ((key == KeyEvent.VK_W) && (rotation != 2)) {
+            rotation = 1;
+            System.out.println("penis");
+        } else if (key == KeyEvent.VK_A && (rotation != 3)) {
+            rotation = 4;
+            System.out.println("penis12");
+        } else if (key == KeyEvent.VK_S && (rotation != 1)) {
+            rotation = 2;
+            System.out.println("penis23");
+        } else if (key == KeyEvent.VK_D && (rotation != 4)) {
+            rotation = 3;
+            System.out.println("penis34");
         }
-        if (key == KeyEvent.VK_A && (right = false)) {
-            left = true;
-            up = false;
-            down = false;
-        }
-        if (key == KeyEvent.VK_S && (up = false)) {
-            left = false;
-            right = false;
-            down = true;
-        }
-        if (key == KeyEvent.VK_D && (left = false)) {
-
-            right = false;
-            up = false;
-            down = true;
-        }
-
-        while (up = true) {
-            drawSnakeClass.headY = drawSnakeClass.headY - drawSnakeClass.speed;
-        }
+        /*
+         * while (rotation == 1) {
+         * drawSnakeClass.headY -= drawSnakeClass.headY;
+         * }
+         * while (rotation == 2) {
+         * drawSnakeClass.headY += drawSnakeClass.headY;
+         * }
+         * while (rotation == 3) {
+         * drawSnakeClass.headX += drawSnakeClass.headX;
+         * }
+         * while (rotation == 4) {
+         * drawSnakeClass.headX -= drawSnakeClass.headX;
+         * }
+         */
     }
 }
